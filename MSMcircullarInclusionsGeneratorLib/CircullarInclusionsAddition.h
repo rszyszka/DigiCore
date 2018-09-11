a6a23ed9359a4862415f5590c3d2ba2f969df6f1
Rafal Szyszka <sysecka@gmail.com>
2018-09-11 20:47:27 +0200

    circullar inclusions v1

--- MSMcircullarInclusionsGeneratorLib/CircullarInclusionsAddition.h
+++ MSMcircullarInclusionsGeneratorLib/CircullarInclusionsAddition.h
@@ -25,16 +25,17 @@ private:
 	void setBalls3D();
 	void adjustSurface();
 	void mixBalls2D();
-	void mixBals3D();
+	void mixBalls3D();
 	void reduceBalls3D();
 	bool checkIfInRange(Ball movedBall, Direction dir);
 	bool checkIfInRange(Ball movedBall);
 	void fill2dSpace();
+	void fill3dSpace();
 	BallSchema2D* create2DSchema(int radius);
 	void fill2dBall(BallSchema2D*, int x, int y, int radius);
-	void ballDirectionInitializer(Ball ball);
-	void directionSetter(Ball ball, Ball notMovedBall);
-	void moveBall(Ball ball);
+	void ballDirectionInitializer(Ball &ball);
+	void directionSetter(Ball &ball, Ball &notMovedBall);
+	void moveBall(Ball &ball);
 public:
 	__declspec(dllexport) CircullarInclusionsAddition(Space *space);
 	__declspec(dllexport) bool performStep() override;
