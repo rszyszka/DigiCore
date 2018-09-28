6e15f528ac6e78146931080732f61ceae098a9f7
Rafał Szyszka <sysecka@gmail.com>
2018-09-28 10:11:53 +0200

    titan lib v1

--- MSMtitaniumLib/Titanium.cpp
+++ MSMtitaniumLib/Titanium.cpp
@@ -1,8 +1,70 @@
 #include "Titanium.h"
+#include <iostream>
+#include <ctime>
+#include <map>
 
-Titanium::Titanium(Space * space) : GrainGrowth(space)
+void Titanium::updateSpace() const
+{
+	for (unsigned int i = 0; i<xSize; i++)
+	{
+		for (unsigned int j = 0; j<ySize; j++)
+		{
+			for (unsigned int k = 0; k<zSize; k++)
+			{
+				auto cell = space->getCells()[i][j][k];
+				auto nextStepCell = nextStepSpace->getCells()[i][j][k];
+
+				cell->setPropertiesFromOtherCell(nextStepCell);
+			}
+		}
+	}
+}
+
+Cell * Titanium::getMostFrequentValue(Cell ** cells) const
+{
+
+	/* TODOOOO:
+
+	map<int, int> cellsOfGrainNr;
+
+	for (int i = 0; i < space->getNeighborhood()->getNeighboursSize(); i++)
+	{
+		auto id = cells[i]->getId();
+		auto canGrowth = cells[i]->getCanGrowth();
+		if (id == 0 || !canGrowth)
+			continue;
+
+		if (cellsOfGrainNr.find(id) != cellsOfGrainNr.end()) //already exists.. increment then
+			cellsOfGrainNr[id]++;
+		else
+			cellsOfGrainNr.emplace(id, 1);
+	}
+
+	int mostCommonNeighCounter = 0;
+	int IdOfMostCommonCounter = 0;
+
+	for (auto& x : cellsOfGrainNr) //wyznaczamy maxa
+	{
+		if (x.second > mostCommonNeighCounter)
+		{
+			IdOfMostCommonCounter = x.first;
+			mostCommonNeighCounter = x.second;
+		}
+	}
+
+	for (int i = 0; i < space->getNeighborhood()->getNeighboursSize(); i++)
+	{
+		if (cells[i]->getId() == IdOfMostCommonCounter)
+			return cells[i];
+	}
+	*/
+	return nullptr;
+}
+
+Titanium::Titanium(Space * space) : Simulation(space)
 {
 	spaceToRecover = new Space(this->space);
+	nextStepSpace = new Space(this->space);
 
 	for (unsigned int i = 0; i < xSize; i++)
 	{
@@ -18,6 +80,8 @@ Titanium::Titanium(Space * space) : GrainGrowth(space)
 
 bool Titanium::performStep()
 {
+	//TODO:
+
 	bool changed = false;
 	int progress_counter = 0;
 
@@ -63,4 +127,9 @@ bool Titanium::performStep()
 	progress = 100 * progress_counter / (xSize * ySize * zSize);
 
 	return changed;
+}
+
+int Titanium::getProgress()
+{
+	return progress;
 }
--- MSMtitaniumLib/Titanium.h
+++ MSMtitaniumLib/Titanium.h
@@ -1,11 +1,18 @@
 #pragma once
-#include "../MSMgrainGrowthLib/GrainGrowth.h"
+#include "../MSMcoreLib/Simulation.h"
 
-class Titanium : public GrainGrowth
+class Titanium : public Simulation
 {
 private:
 	Space* spaceToRecover;
+	Space* nextStepSpace;
+	int stepCounter;
+	int progress;
+
+	void updateSpace() const;
+	Cell* getMostFrequentValue(Cell** cells) const;
 public:
 	__declspec(dllexport) Titanium(Space *space);
 	__declspec(dllexport) bool performStep() override;
+	__declspec(dllexport) int getProgress() override;
 };
